740a6636b0d6c307c4b4dd16ec12e642434c230f
Manuela Bertella Ossanes <20212PF.CC0010@passofundo.ifsul.local>
2023-10-09 08:27:29 -0300

    main

--- src/componentes/telas/Home.jsx
+++ src/componentes/telas/Home.jsx
@@ -6,7 +6,7 @@ function Home(){
 
           <h4 id="txtCont2">Esta página representa o primeiro trabalho prático proposto na matéria de Programação Para WEB.</h4>
           <h4 id="txtCont2">O projeto representa uma empresa que possui jogos cadastrados 
-          e que podem ser acessador por jogadores (Essa parte ainda não está feita).
+          e que, na teoria, podem ser acessador por jogadores.
           Cada jogo possui sua categoria, plataforma e desenvolvedora.</h4>
         </div>
       );
